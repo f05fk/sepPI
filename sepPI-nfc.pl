7cfbb12fd5f78d7a73da8c59e4d0403a3d5f0f4c
Claus Schrammel <claus@f05fk.net>
2019-02-01 12:37:42 +0100

    updated copyright header

--- sepPI-nfc.pl
+++ sepPI-nfc.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -I /home/pi/sepPI
 #########################################################################
-# Copyright (C) 2017-2018 Claus Schrammel <claus@f05fk.net>             #
+# Copyright (C) 2017-2019 Claus Schrammel <claus@f05fk.net>             #
 #                                                                       #
 # This program is free software: you can redistribute it and/or modify  #
 # it under the terms of the GNU General Public License as published by  #
